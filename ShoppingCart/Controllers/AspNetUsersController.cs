69b14d8a2384ee51ec0123798dbfa55246edc4b8
s294779 <bbstha143@gmail.com>
2017-05-26 14:22:57 +0930

    With User details and change password with some changes in home page

---
+++ ShoppingCart/Controllers/AspNetUsersController.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.AspNet.Identity;
+using ShoppingCart;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCart.Controllers
+{
+    public class AspNetUsersController : Controller
+    {
+        private OnlineShoppingDBEntities db = new OnlineShoppingDBEntities();
+
+        // GET: AspNetUsers
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserName();
+
+            return View(db.AspNetUsers.Where(x => x.UserName == userId));
+        }
+
+        // GET: AspNetUsers/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetUser aspNetUser = db.AspNetUsers.Find(id);
+            if (aspNetUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspNetUser);
+        }
+
+        // GET: AspNetUsers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AspNetUsers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Age,Address,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] AspNetUser aspNetUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AspNetUsers.Add(aspNetUser);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "EmailConfirmed", "PasswordHash", "SecurityStamp", aspNetUser.Id);
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "PhoneNumber", "PhoneNumberConfirmed", aspNetUser.Id);
+            ViewBag.UserID = new SelectList(db.AspNetUsers,"TwoFactorEnabled", "LockoutEndDateUtc", "LockoutEnabled", "AccessFailedCount", "UserName", aspNetUser.Id);
+           
+            return View(aspNetUser);
+        }
+
+        // GET: AspNetUsers/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetUser aspNetUser = db.AspNetUsers.Find(id);
+            if (aspNetUser == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "EmailConfirmed", "PasswordHash", "SecurityStamp", aspNetUser.Id);
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "PhoneNumber", "PhoneNumberConfirmed", aspNetUser.Id);
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "TwoFactorEnabled", "LockoutEndDateUtc", "LockoutEnabled", "AccessFailedCount", "UserName", aspNetUser.Id);
+
+            return View(aspNetUser);
+        }
+
+        // POST: AspNetUsers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Name,Age,Address,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] AspNetUser aspNetUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aspNetUser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id","Name", "EmailConfirmed", "PasswordHash", "SecurityStamp", aspNetUser.Id);
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "PhoneNumber", "PhoneNumberConfirmed", aspNetUser.Id);
+            ViewBag.UserID = new SelectList(db.AspNetUsers,"TwoFactorEnabled", "LockoutEndDateUtc", "LockoutEnabled", "AccessFailedCount", "UserName", aspNetUser.Id);
+           
+            return View(aspNetUser);
+        }
+
+        // GET: AspNetUsers/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetUser aspNetUser = db.AspNetUsers.Find(id);
+            if (aspNetUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspNetUser);
+        }
+
+        // POST: AspNetUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            AspNetUser aspNetUser = db.AspNetUsers.Find(id);
+            db.AspNetUsers.Remove(aspNetUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- ShoppingCart/Controllers/ManageController.cs
+++ ShoppingCart/Controllers/ManageController.cs
@@ -238,7 +238,7 @@ namespace ShoppingCart.Controllers
                 {
                     await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
                 }
-                return RedirectToAction("Index", new { Message = ManageMessageId.ChangePasswordSuccess });
+                return RedirectToAction("Index","AspNetUsers", new { Message = ManageMessageId.ChangePasswordSuccess });
             }
             AddErrors(result);
             return View(model);
